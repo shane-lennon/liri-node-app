0754b49af00f7014634c6e9451b95b3674664555
Shane Lennon <shane.michael.lennon@gmail.com>
2018-12-09 22:34:40 -0600

    Added movie-this functionality

--- liri.js
+++ liri.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('moment')
 var axios = require('axios');
 var keys = require('./keys');
 var Spotify = require('node-spotify-api');
@@ -12,6 +13,7 @@ console.log('\n')
 
 // executes various options based on input command
 switch (command) {
+
     case 'spotify-this-song':
         // adds default in case of no input
         if (param == '') {
@@ -31,6 +33,32 @@ switch (command) {
             console.log('URL:    ' + data.tracks.items[0].external_urls.spotify);
         });
         break;
+
     case 'movie-this':
+        // Enters default paramater if no input
+        if (param == '') {
+            param = 'Mr Nobody'
+        }
+        // Search OMDB with parameters
+        axios.get("http://www.omdbapi.com/?t=" + param + "&y=&plot=short&apikey=trilogy")
+            .then(function (response) {
+                console.log('Title:        ' + response.data.Title);
+                console.log('Year:         ' + response.data.Year);
+                console.log('IMDB Rating:  ' + response.data.Ratings[0].Value);
+                console.log('Tomato-meter: ' + response.data.Ratings[1].Value);
+                console.log('Country:      ' + response.data.Country);
+                console.log('Plot:         ' + response.data.Plot);
+                console.log('Actors:       ' + response.data.Actors);
+            });
+        break;
+
+    case 'concert-this':
+        // Search bandsintown with parameters
+        axios.get("https://rest.bandsintown.com/artists/" + param + "/events?app_id=codingbootcamp")
+            .then(function (response) {
+                console.log(response.data[0].venue.city);
+                console.log(moment(response.data[0].datetime).date);
+                // console.log('IMDB Rating:  ' + response.data.Ratings[0].Value);
+            });
 
 }
