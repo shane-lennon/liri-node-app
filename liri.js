54e8805cc6e7b6a419edb05b93ee8854e89a47e3
Shane Lennon <shane.michael.lennon@gmail.com>
2018-12-09 21:19:20 -0600

    Added spotify functionality

--- liri.js
+++ liri.js
@@ -1,5 +1,36 @@
-require("dotenv").config();
+require('dotenv').config();
+var axios = require('axios');
+var keys = require('./keys');
+var Spotify = require('node-spotify-api');
 
 var spotify = new Spotify(keys.spotify);
+// stores third command line argument as 'command'
+var command = process.argv[2];
+// stores remaining command line arguments as a space delimited string 'param'
+var param = process.argv.splice(3, process.argv.length - 1).join(' ');
+console.log('\n')
 
-console.log(spotify.keys);
+// executes various options based on input command
+switch (command) {
+    case 'spotify-this-song':
+        // adds default in case of no input
+        if (param == '') {
+            param = 'ace of base the sign';
+        }
+        // search spotify for track, displaying info for first result
+        spotify.search({
+            type: 'track',
+            query: param
+        }, function (err, data) {
+            if (err) {
+                return console.log('Error occurred: ' + err);
+            }
+            console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+            console.log('Album:  ' + data.tracks.items[0].album.name);
+            console.log('Song:   ' + data.tracks.items[0].name);
+            console.log('URL:    ' + data.tracks.items[0].external_urls.spotify);
+        });
+        break;
+    case 'movie-this':
+
+}
